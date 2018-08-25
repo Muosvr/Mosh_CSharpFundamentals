349cf04be0ad4cb91487181e592d268a907b6a09
Muosvr <lujason2015@gmail.com>
2018-08-25 12:59:19 -0400

    File, Directories, path ex2

--- 075Exercise/075Exercise/Program.cs
+++ 075Exercise/075Exercise/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace _075Exercise
 {
@@ -7,18 +8,38 @@ namespace _075Exercise
     {
         static void Main(string[] args)
         {
+            var path = @"d:\CareerDev\Mosh_CSharpFundamentals\075Exercise\Hello.txt";
             Program exercise = new Program();
-            exercise.Exercise1();
+            //exercise.Exercise1(path);
+            Console.WriteLine("The longest word is " + exercise.Exercise2(path));
         }
 
-        public void Exercise1()
+        public void Exercise1(string path)
         {
-            //var input = File.ReadAllText(@"d:\CareerDev\Mosh_CSharpFundamentals\Hello.txt");
-            //var array = input.Split(' ');
+            var file = File.ReadAllText(path);
+            var array = file.Split(' ');
             //var input = File.Exists(@"d:\CareerDev\Mosh_CSharpFundamentals\Hello.txt");
 
-            //Console.WriteLine("There are {0} words in this file", array.Length);
-            Console.WriteLine(input);
+            Console.WriteLine("There are {0} words in this file", array.Length);
+            //Console.WriteLine(input);
+        }
+
+        public string Exercise2(string path)
+        {
+            var file = File.ReadAllText(path);
+            var array = file.Split(' ');
+            var longestWord = new StringBuilder();
+            
+            foreach (string word in array)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord.Clear();
+                    longestWord.Append(word);
+                }  
+            }
+
+            return longestWord.ToString();
         }
     }
 }
